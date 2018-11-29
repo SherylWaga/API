ec0e46662a1cb4eeb3576e3750035fc71787181a
SherylWaga <sherylwaga@gmail.com>
2018-11-30 01:30:06 +0300

    [Finish #162229785]view all redflags

--- app/api/v1/views/redflag_views.py
+++ app/api/v1/views/redflag_views.py
@@ -3,26 +3,9 @@ from flask_restful import Resource
 #local import
 from app.api.v1.models.redflag_models import Locate
 class Redflags(Resource):
+    def get(self):
+        redflags = Locate().all()
+        return redflags
     def post(self):
         new_post= Locate().new()
         return new_post
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
