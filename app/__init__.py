e51def378d914644cc2db6dde5855b552b995cc7
SherylWaga <sherylwaga@gmail.com>
2018-11-30 10:31:05 +0300

    [Finish #162229797]Finish view specific redflag

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from flask_restful import Api,Resource
 #local imports
 
 
-from .api.v1.views.redflag_views import Redflags
+from  .api.v1.views.redflag_views import Redflags,RedflagsSpecific
 
 def create_app():
     app=Flask(__name__)
     api = Api(app)
     api.add_resource(Redflags,'/redflags')
-
+    api.add_resource(RedflagsSpecific,'/redflags/<int:item_id>')
     return app
--- app/api/v1/views/redflag_views.py
+++ app/api/v1/views/redflag_views.py
@@ -9,3 +9,31 @@ class Redflags(Resource):
     def post(self):
         new_post= Locate().new()
         return new_post
+class RedflagsSpecific(Resource):
+    def get(self, item_id):
+        specific_data = Locate().specific(item_id)
+        return specific_data
+    def delete(self,item_id):
+        return Locate().delete_record(item_id)
+    def put(self,item_id):
+        return Locate().edit_record(item_id)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
