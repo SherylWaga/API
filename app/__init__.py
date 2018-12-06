9604693ca9af59cdac81ec48d15b08f165ca9fdf
SherylWaga <sherylwaga@gmail.com>
2018-12-06 11:44:46 +0300

    [Chore #162445846]Finish  database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint
 from .api.v1 import version_1 as v1
+#local imports
 from instance.config import app_config
-# from .db_config import create_tables
+from .db_config import create_tables
 
 
 def create_app(config_name='development'):
@@ -10,7 +11,7 @@ def create_app(config_name='development'):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
-    # create-tables()
+    create_tables()
 
     app.register_blueprint(v1)
     return app
